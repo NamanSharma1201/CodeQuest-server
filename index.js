15b50692c8cc15f67158fae8238d1a5e3eb02391
Naman Sharma <120219878+NamanSharma1201@users.noreply.github.com>
2024-07-19 21:20:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,16 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { createServer } from "http";
-import { Server } from "socket.io";
 import connectDB from "./config/connectDB.js";
 import userRouter from "./routes/user.js";
 import problemRouter from "./routes/problem.js";
 import commentRouter from "./routes/comments.js";
 import blogRouter from "./routes/blogs.js";
+
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST"],
-  },
-});
 
 connectDB();
 
@@ -33,18 +25,6 @@ app.use("/api/problem", problemRouter);
 app.use("/blog", blogRouter);
 app.use("/comment", commentRouter);
 
-// Socket.io connection
-io.on("connection", (socket) => {
-  // console.log("A user connected");
-
-  socket.on("sendMessage", (message) => {
-    const { name, message: msg } = message;
-    io.emit("receiveMessage", { name, message: msg });
-  });
-
-  socket.on("disconnect", () => {});
-});
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port} ....`);
+app.listen(port, () => {
+  console.log(`API server running on port ${port} ....`);
 });
